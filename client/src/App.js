deb0dd815629cc0a4ab19d18db1d7839da07c59c
unknown <josephlee45@gmail.com>
2018-07-14 23:14:57 -0700

    update sample posts notes and css

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       webSocket: new WebSocket('ws://159.89.144.153:8001')
     }
   }
-
+  //webSocket: new WebSocket('ws://localhost:8001')
 
   componentDidMount() {
     var self = this;
@@ -26,16 +26,16 @@ class App extends Component {
       self.setState({venmoPosts});
     }
 
-    addPost("Sample User 1", "06-28-2018", "yo i wanna give a shoutout to my boy fred", ".10", "https://media.istockphoto.com/photos/red-apple-picture-id495878092?k=6&m=495878092&s=612x612&w=0&h=q9k5jN-1giBGZgTM6QhyKkPqtGf6vRpkgDzAwEz9DkY=");
-    addPost("Sample User 2", "06-28-2018", "yo i wanna give a shoutout to my boy tom", ".10", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMmZvlOjc6450J56KEHL83Qs8JUYhbddubOJAQaLO5InXD0PddLw");
-    addPost("Sample User 3", "06-28-2018", "yo i wanna give a shoutout to my boy cam", ".10", "https://icdn3.digitaltrends.com/image/walmart-cyber-monday-720x720.jpg?ver=1.jpg");
-    addPost("Sample User 4", "06-28-2018", "yo i wanna give a shoutout to my boy tim", ".10", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMmZvlOjc6450J56KEHL83Qs8JUYhbddubOJAQaLO5InXD0PddLw");
-    addPost("Sample User 5", "06-28-2018", "yo i wanna give a shoutout to my boy mike", ".10", "https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg");
+    addPost("Sample User 1", "06-28-2018", "I want to give a shoutout to Steve", ".10", "https://media.istockphoto.com/photos/red-apple-picture-id495878092?k=6&m=495878092&s=612x612&w=0&h=q9k5jN-1giBGZgTM6QhyKkPqtGf6vRpkgDzAwEz9DkY=");
+    addPost("Sample User 2", "06-28-2018", "Happy Birthday Mom!", ".10", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMmZvlOjc6450J56KEHL83Qs8JUYhbddubOJAQaLO5InXD0PddLw");
+    addPost("Sample User 3", "06-28-2018", "Today is a great day :)", ".10", "https://icdn3.digitaltrends.com/image/walmart-cyber-monday-720x720.jpg?ver=1.jpg");
+    addPost("Sample User 4", "06-28-2018", "Hellooo", ".10", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMmZvlOjc6450J56KEHL83Qs8JUYhbddubOJAQaLO5InXD0PddLw");
+    addPost("Sample User 5", "06-28-2018", "Shoutout to my friends in Orange county!", ".10", "https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg");
 
 
     var webSocket = this.state.webSocket;
     webSocket.onclose = function(event){
-      console.log("yo i got closed!");
+      console.log("i got closed!");
     }.bind(this);
 
     webSocket.onmessage = function(event){
